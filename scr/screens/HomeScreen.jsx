81dcc5904029e3cd88bbe6012f3d2916f374c91f
nishant-kd <ndk6830@gmail.com>
2023-11-24 22:28:06 -0700

    lab 5 again

--- scr/screens/HomeScreen.jsx
+++ scr/screens/HomeScreen.jsx
@@ -1,28 +1,18 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
+import React from 'react';
+import { Button } from 'react-native';
+import MainLayout from '../layouts/MainLayout';
 import ToDoList from '../components/ToDoList';
 import ToDoForm from '../components/ToDoForm';
-import MainLayout from '../layouts/MainLayout';
 
-function HomeScreen({navigation}) {
-  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
-
-  const addTask = taskText => {
-    if (taskText && !tasks.includes(taskText)) {
-      setTasks([...tasks, taskText]);
-    }
-  };
-
+function HomeScreen({ navigation }) {
   return (
     <MainLayout>
-      <SafeAreaView>
-        <ToDoList tasks={tasks} />
-        <ToDoForm addTask={addTask} />
-        <Button
-          title="Go to About"
-          onPress={() => navigation.navigate('About')}
-        />
-      </SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate('About')}
+      />
     </MainLayout>
   );
 }
